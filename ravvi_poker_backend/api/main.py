418e3ca3608a9b96246ad91a52cac865abb89c33
Alexander Keda <alexander.keda@ravvi.net>
2023-05-16 12:26:51 +0300

    poker-20: remove outdated code

--- ravvi_poker_backend/api/main.py
+++ ravvi_poker_backend/api/main.py
@@ -2,13 +2,11 @@
 from fastapi import FastAPI, APIRouter
 from . import auth
 from . import user
-#from . import ws_test
 from . import ws_test2
 
 v1 = APIRouter(prefix="/v1")
 v1.include_router(auth.router)
 v1.include_router(user.router)
-#v1.include_router(ws_test.router)
 v1.include_router(ws_test2.router)
 
 app = FastAPI()
