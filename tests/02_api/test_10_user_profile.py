1dcbe8e4364f1d0a4f20cd98e84acad81e31367f
Stanislav Chuprinskiy <stanislav.chuprinskiy@ravvi.net>
2023-06-28 15:18:40 +0300

    Add user_profile_email test

--- tests/02_api/test_10_user_profile.py
+++ tests/02_api/test_10_user_profile.py
@@ -25,6 +25,7 @@ def test_user_profile():
     profile1 = response.json()
     assert profile1["id"]
     assert profile1["username"] == username
+    assert profile1["email"] is None
     assert profile1["has_password"] == False
 
     # set password
@@ -38,4 +39,34 @@ def test_user_profile():
     profile2 = response.json()
     assert profile2["id"] == profile1["id"]
     assert profile2["username"] == username
+    assert profile2["email"] is None
     assert profile2["has_password"] == True
+
+
+def test_user_profile_email():
+    # register new guest
+    access_token, username = register_guest()
+    headers = {"Authorization": "Bearer " + access_token}
+
+    # check profile
+    response = client.get("/v1/user/profile", headers=headers)
+    assert response.status_code == 200
+    profile1 = response.json()
+    assert profile1["id"]
+    assert profile1["username"] == username
+    assert profile1["email"] is None
+    assert profile1["has_password"] == False
+
+    # set email
+    params = dict(email="test_email@test.ru")
+    response = client.post("/v1/user/profile/email", headers=headers, json=params)
+    assert response.status_code == 200
+
+    # check profile
+    response = client.get("/v1/user/profile", headers=headers)
+    assert response.status_code == 200
+    profile2 = response.json()
+    assert profile2["id"] == profile1["id"]
+    assert profile2["username"] == username
+    assert profile2["email"] == params["email"]
+    assert profile2["has_password"] == False
