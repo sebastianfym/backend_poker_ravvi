153c1fa67c6df596ea8eb30732cb1c7e9ac042a2
Stanislav Chuprinskiy <stanislav.chuprinskiy@ravvi.net>
2023-06-29 15:24:24 +0300

    Add base64 photo upload in user_update route

--- ravvi_poker/api/user.py
+++ ravvi_poker/api/user.py
@@ -1,8 +1,16 @@
+import base64
+import uuid
+from pathlib import Path
+
+import aiofiles
 from fastapi import APIRouter
 from pydantic import BaseModel, EmailStr
 
 from ..db.dbi import DBI
 from .auth import RequireSessionUUID, get_session_and_user
+
+BASE_PATH = Path(__file__).parent
+USER_PHOTO_PATH = BASE_PATH.joinpath("user_photos")
 
 router = APIRouter(prefix="/user", tags=["user"])
 
@@ -12,6 +20,7 @@ class UserProfile(BaseModel):
     username: str
     email: EmailStr | None
     has_password: bool
+    photo: str | None
 
 
 class UserEmail(BaseModel):
@@ -19,7 +28,21 @@ class UserEmail(BaseModel):
 
 
 class UserUpdateFields(BaseModel):
-    username: str
+    username: str | None
+    photo: str | None
+
+
+async def get_user_profile(user) -> UserProfile:
+    photo = await return_base64_photo(user.photo) if user.photo else user.photo
+    has_password = bool(user.password_hash)
+
+    return UserProfile(
+        id=user.id,
+        username=user.username,
+        email=user.email,
+        has_password=has_password,
+        photo=photo,
+    )
 
 
 @router.get("/profile")
@@ -27,26 +50,60 @@ async def v1_get_user_profile(session_uuid: RequireSessionUUID):
     with DBI() as dbi:
         _, user = get_session_and_user(dbi, session_uuid)
 
-    return UserProfile(
-        id=user.id,
-        username=user.username,
-        email=user.email,
-        has_password=bool(user.password_hash)
-    )
+    return await get_user_profile(user)
 
 
-@router.put("/profile")
-async def v1_update_user_profile(params: UserUpdateFields, session_uuid: RequireSessionUUID):
+async def save_base64_photo(base64_photo: str,
+                            upload_path: Path = USER_PHOTO_PATH) -> str:
+    """Сохраняет base64 фотографии
+
+    Возвращает сгенерированное название фото.
+    """
+    bytes_photo = base64.b64decode(base64_photo.encode())
+    photo_name = str(uuid.uuid4())
+    full_photo_path = upload_path.joinpath(photo_name)
+    async with aiofiles.open(full_photo_path, "wb") as photo:
+        await photo.write(bytes_photo)
+    return photo_name
+
+
+async def return_base64_photo(photo_name: str,
+                              upload_path: Path = USER_PHOTO_PATH) -> str:
+    """Возвращает фото в формате base64"""
+    full_photo_path = upload_path.joinpath(photo_name)
+    async with aiofiles.open(full_photo_path, "rb") as photo:
+        base64_photo = base64.b64encode(await photo.read())
+    return base64_photo
+
+
+async def update_user_photo(user, base64_photo, upload_path=USER_PHOTO_PATH):
+    if user.photo:
+        full_old_photo_path = USER_PHOTO_PATH.joinpath(user.photo)
+        full_old_photo_path.unlink(missing_ok=True)
+
+    return await save_base64_photo(base64_photo)
+
+
+@router.patch("/profile")
+async def v1_update_user_profile(params: UserUpdateFields,
+                                 session_uuid: RequireSessionUUID):
     with DBI() as dbi:
         _, user = get_session_and_user(dbi, session_uuid)
-        user = dbi.update_user(user.id, params.username)
 
-    return UserProfile(
-        id=user.id,
-        username=user.username,
-        email=user.email,
-        has_password=bool(user.password_hash)
-    )
+        if params.photo:
+            params.photo = await update_user_photo(user, params.photo)
+        for field in UserUpdateFields.__fields__.keys():
+            field_value = getattr(params, field, None)
+            field_value = field_value if field_value else getattr(user, field)
+            setattr(params, field, field_value)
+        user = dbi.update_user(user.id, params.username, params.photo)
+
+    return await get_user_profile(user)
+
+
+@router.delete("/profile")
+async def v1_delete_user_profile(session_uuid: RequireSessionUUID):
+    pass
 
 
 @router.post("/profile/email")
@@ -55,9 +112,4 @@ async def v1_set_user_email(params: UserEmail, session_uuid: RequireSessionUUID)
         _, user = get_session_and_user(dbi, session_uuid)
         user = dbi.update_user_email(user.id, params.email)
 
-    return UserProfile(
-        id=user.id,
-        username=user.username,
-        email=user.email,
-        has_password=bool(user.password_hash)
-    )
+    return await get_user_profile(user)
