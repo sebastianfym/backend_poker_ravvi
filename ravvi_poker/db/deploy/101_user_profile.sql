c42b3361de99dab83dcf1621a2e2c2a889b3718d
Stanislav Chuprinskiy <stanislav.chuprinskiy@ravvi.net>
2023-06-30 16:46:35 +0300

    Change profile photo type to bytea in db

--- ravvi_poker/db/deploy/101_user_profile.sql
+++ ravvi_poker/db/deploy/101_user_profile.sql
@@ -4,7 +4,7 @@ create table user_profile (
     username varchar(100) default null,
     email varchar(100) default null,
     password_hash varchar(100) default null,
-    photo varchar(100) default null,
+    photo bytea default null,
     created_ts timestamp not null default NOW(),
     closed_ts  timestamp default NULL
 );
