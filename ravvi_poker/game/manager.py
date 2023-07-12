b736a91717fccd1469beaa6f6b29e410c64b92ca
Alexander Keda <alexander.keda@ravvi.net>
2023-07-12 18:15:52 +0300

    bugfix: add_table() typo

--- ravvi_poker/game/manager.py
+++ ravvi_poker/game/manager.py
@@ -53,7 +53,7 @@ class Manager(Logger_MixIn):
                 game_type = 'PLO' if table_row.id==2 else 'NLH'
             if game_type=='PLO':
                 n_seats = min(n_seats, 6)
-            table = Table(table.id, game_type=game_type, n_seats=n_seats)
+            table = Table(table_row.id, game_type=game_type, n_seats=n_seats)
             self.tables[table.id] = table
         except Exception as ex:
             self.log_exception("add_table %s: %s", table_row, ex)
