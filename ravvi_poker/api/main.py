7cf9025f572b26dad8c2ecee8280d519b6ad78af
Stanislav Chuprinskiy <stanislav.chuprinskiy@ravvi.net>
2023-07-12 14:05:07 +0300

    Add cors origins

--- ravvi_poker/api/main.py
+++ ravvi_poker/api/main.py
@@ -1,5 +1,7 @@
 
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import auth
 from . import user
 from . import images
@@ -18,4 +20,20 @@ v1.include_router(ws.router)
 v1.include_router(ws_test2.router)
 
 app = FastAPI()
+
+origins = [
+    "http://80.250.80.58",
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://0.0.0.0",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(v1)
