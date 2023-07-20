25158172349ce11c7b44fca17f9e7407ac78c951
Stanislav Chuprinskiy <stanislav.chuprinskiy@ravvi.net>
2023-07-20 15:38:37 +0300

    Make table name field not required

--- ravvi_poker/api/tables.py
+++ ravvi_poker/api/tables.py
@@ -12,7 +12,7 @@ router = APIRouter(prefix="/tables", tags=["tables"])
 
 
 class TableCreate(BaseModel):
-    table_name: str
+    table_name: str | None = None
     table_type: str | None = None
     table_seats: int
     game_type: str
@@ -29,20 +29,13 @@ class TableProfile(BaseModel):
     game_subtype: str | None
 
 
-class TableProfileList(BaseModel):
-    tables: list[TableProfile]
-
-
-@clubs_router.get("/{club_id}/tables", response_model=TableProfileList, status_code=200,
-                  summary="Get club tables")
+@clubs_router.get("/{club_id}/tables", status_code=200, summary="Get club tables")
 async def v1_get_club_tables(club_id: int, session_uuid: RequireSessionUUID):
     with DBI() as dbi:
         _, user = get_session_and_user(dbi, session_uuid)
         club = dbi.get_club(club_id)
         if not club:
-            raise HTTPException(
-                status_code=HTTP_404_NOT_FOUND, detail="Club not found"
-            )
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Club not found")
         club_member = dbi.get_club_member(club.id, user.id)
         if not club_member:
             raise HTTPException(
@@ -50,7 +43,7 @@ async def v1_get_club_tables(club_id: int, session_uuid: RequireSessionUUID):
             )
         tables = dbi.get_tables_for_club(club_id=club_id)
 
-    return TableProfileList(tables=[
+    return list([
         TableProfile(
             id=table.id,
             club_id=table.club_id,
@@ -63,21 +56,15 @@ async def v1_get_club_tables(club_id: int, session_uuid: RequireSessionUUID):
     ])
 
 
-@clubs_router.post("/{club_id}/tables", response_model=TableProfile, status_code=201,
-                   summary="Create club table")
-async def v1_create_club_table(club_id: int, params: TableCreate,
-                               session_uuid: RequireSessionUUID):
+@clubs_router.post("/{club_id}/tables", status_code=201, summary="Create club table")
+async def v1_create_club_table(club_id: int, params: TableCreate, session_uuid: RequireSessionUUID):
     with DBI() as dbi:
         _, user = get_session_and_user(dbi, session_uuid)
         club = dbi.get_club(club_id)
         if not club:
-            raise HTTPException(
-                status_code=HTTP_404_NOT_FOUND, detail="Club not found"
-            )
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Club not found")
         if club.founder_id != user.id:
-            raise HTTPException(
-                status_code=HTTP_403_FORBIDDEN, detail="Permission denied"
-            )
+            raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="Permission denied")
         table = dbi.create_table(club_id=club_id, **params.model_dump())
 
     return TableProfile(
@@ -91,26 +78,18 @@ async def v1_create_club_table(club_id: int, params: TableCreate,
     )
 
 
-@clubs_router.delete("/{club_id}/tables/{table_id}", response_model={}, status_code=204,
-                     summary="Delete club table")
-async def v1_delete_club_table(club_id: int, table_id: int,
-                               session_uuid: RequireSessionUUID):
+@clubs_router.delete("/{club_id}/tables/{table_id}", status_code=204, summary="Delete club table")
+async def v1_delete_club_table(club_id: int, table_id: int, session_uuid: RequireSessionUUID):
     with DBI() as dbi:
         _, user = get_session_and_user(dbi, session_uuid)
         club = dbi.get_club(club_id)
         if not club:
-            raise HTTPException(
-                status_code=HTTP_404_NOT_FOUND, detail="Club not found"
-            )
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Club not found")
         if club.founder_id != user.id:
-            raise HTTPException(
-                status_code=HTTP_403_FORBIDDEN, detail="Permission denied"
-            )
+            raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="Permission denied")
         table = dbi.get_table(table_id)
         if not table or table.club_id != club.id:
-            raise HTTPException(
-                status_code=HTTP_404_NOT_FOUND, detail="Table not found"
-            )
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Table not found")
         dbi.delete_table(table.id)
 
     return {}
