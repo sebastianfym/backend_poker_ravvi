6d457745daa280b35948d2d951a13cc2ed65ef16
Stanislav Chuprinskiy <stanislav.chuprinskiy@ravvi.net>
2023-07-25 17:22:43 +0300

    Add email update endpoint

--- ravvi_poker/api/user.py
+++ ravvi_poker/api/user.py
@@ -17,16 +17,19 @@ class UserProfile(BaseModel):
     image_id: int | None = None
 
 
-class UserProfileUpdate(BaseModel):
-    username: str | None = None
-    email: EmailStr | None = None
-    image_id: int | None = None
-
-
 class UserProfileInfo(BaseModel):
     id: int
     username: str | None = None
     image_id: int | None = None
+
+
+class UserProps(BaseModel):
+    username: str | None = None
+    image_id: int | None = None
+
+
+class UserEmail(BaseModel):
+    email: EmailStr
 
 
 @router.get("/profile", summary="Get user")
@@ -55,7 +58,7 @@ async def v1_deactivate_user(session_uuid: RequireSessionUUID):
 
 
 @router.patch("/profile", summary="Update user")
-async def v1_update_user(params: UserProfileUpdate, session_uuid: RequireSessionUUID):
+async def v1_update_user(params: UserProps, session_uuid: RequireSessionUUID):
     """Update user"""
     with DBI() as dbi:
         _, user = get_session_and_user(dbi, session_uuid)
@@ -76,16 +79,33 @@ async def v1_update_user(params: UserProfileUpdate, session_uuid: RequireSession
     )
 
 
+@router.post("/profile/email", summary="Set user email")
+async def v1_set_user_email(params: UserEmail, session_uuid: RequireSessionUUID):
+    """Set user email"""
+    with DBI() as dbi:
+        _, user = get_session_and_user(dbi, session_uuid)
+        user = dbi.update_user_profile(user.id, **params.model_dump())
+
+    return UserProfile(
+        id=user.id,
+        username=user.username,
+        email=user.email,
+        has_password=bool(user.password_hash),
+        image_id=user.image_id
+    )
+
+
 @router.get("/{user_id}/profile", summary="Get user info")
 async def v1_get_user_profile(user_id: int, session_uuid: RequireSessionUUID):
     """Get user profile info"""
     with DBI() as dbi:
-        user = dbi.get_user(id=user_id)
-        if not user:
+        _, user = get_session_and_user(dbi, session_uuid)
+        searched_user = dbi.get_user(id=user_id)
+        if not searched_user:
             raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
 
     return UserProfileInfo(
-        id=user.id,
-        username=user.username,
-        image_id=user.image_id
+        id=searched_user.id,
+        username=searched_user.username,
+        image_id=searched_user.image_id
     )
