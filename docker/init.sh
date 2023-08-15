4435262ac98d28f28e1669caf5a22c36780a61f7
Alexander Keda <alexander.keda@ravvi.net>
2023-08-15 16:22:17 +0300

    docker/init add missing build_versioin step

--- docker/init.sh
+++ docker/init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 docker exec ravvi_py bash -lc "pip3 install --upgrade pip"
+docker exec ravvi_py bash -lc "python3 code/build_version.py"
 docker exec ravvi_py bash -lc "pip3 install -e code[tests] --no-build-isolation"
 docker exec ravvi_py bash -lc "ravvi_poker_db drop develop"
 docker exec ravvi_py bash -lc "ravvi_poker_db create develop"
