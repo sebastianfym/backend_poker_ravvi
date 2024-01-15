948d4da28f880f66254da2587e2927cb572bd027
Bogdan Velikorodov <bogdan.velikorodov@ravvi.net>
2024-01-15 16:13:38 +0300

    Добавил manager1 внутрь эндпоинта с сокетом (временное решение, которое помогает убрать дубликаты ответов),вернул ws.manager.start() в app.py

--- ravvi_poker/api/app.py
+++ ravvi_poker/api/app.py
@@ -23,7 +23,7 @@ async def lifespan(app: FastAPI):
     logger.info("api startup")
     #await DBI.pool_open()
     await engine.mamager.start()
-    # await ws.manager.start() #Todo Если все упадет, верни это!
+    await ws.manager.start() #Todo Если все упадет, верни это!
     # statup end
     yield
     # shutdown begin
@@ -52,5 +52,6 @@ v1.include_router(lobby.router)
 v1.include_router(clubs.router)
 v1.include_router(tables.router)
 v1.include_router(ws.router)
+
 
 app.include_router(v1)
