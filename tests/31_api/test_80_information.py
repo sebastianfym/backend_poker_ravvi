97be12171d0a71d9df41271f30441a7da1ab7346
Bogdan Velikorodov <bogdan.velikorodov@ravvi.net>
2024-01-18 13:42:07 +0000

    Update test_80_information.py

--- tests/31_api/test_80_information.py
+++ tests/31_api/test_80_information.py
@@ -7,8 +7,8 @@ def test_get_blind_info(api_client: TestClient, api_guest: UserAccessProfile):
     api_client.headers = {"Authorization": "Bearer " + api_guest.access_token}
 
     blinds_type_dict = {
-        "sng": ["стандарт", "турбо"],
-        "mtt": ["стандарт", "турбо", "гипер турбо"]
+        "sng": ["standard", "turbo"],
+        "mtt": ["standard", "turbo", "hyper_turbo"]
     }
 
     for blind in blinds_type_dict:
@@ -20,17 +20,17 @@ def test_get_blind_info(api_client: TestClient, api_guest: UserAccessProfile):
             assert isinstance(response.json(), list)
 
 
-def test_get_payment_structure(api_client: TestClient, api_guest: UserAccessProfile):
-    # set headers
-    api_client.headers = {"Authorization": "Bearer " + api_guest.access_token}
+# def test_get_payment_structure(api_client: TestClient, api_guest: UserAccessProfile):
+#     # set headers
+#     api_client.headers = {"Authorization": "Bearer " + api_guest.access_token}
 
-    payment_structure_list = [
-        {"players": "1-3", "position": {"first": "100%", "second": "0", "third": "0"}},
-        {"players": "4-6", "position": {"first": "70%", "second": "30%", "third": "0"}},
-        {"players": "7-9", "position": {"first": "50%", "second": "30%", "third": "20%"}}
-    ]
+#     payment_structure_list = [
+#         {"players": "1-3", "position": {"first": "100%", "second": "0", "third": "0"}},
+#         {"players": "4-6", "position": {"first": "70%", "second": "30%", "third": "0"}},
+#         {"players": "7-9", "position": {"first": "50%", "second": "30%", "third": "20%"}}
+#     ]
 
-    response = api_client.get(f"/v1/info/payment structure")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
-    assert response.json() == payment_structure_list
+#     response = api_client.get(f"/v1/info/payment structure")
+#     assert response.status_code == 200
+#     assert isinstance(response.json(), list)
+#     assert response.json() == payment_structure_list
