c4ec98882556ca1acb2447f0ac4aad267f09c96f
Alexander Keda <alexander.keda@ravvi.net>
2024-01-23 19:56:42 +0300

    sng fixes

--- ravvi_poker/engine/clients/ws.py
+++ ravvi_poker/engine/clients/ws.py
@@ -20,25 +20,23 @@ class ClientWS(ClientQueue):
         return self.ws.client_state == WebSocketState.CONNECTED
 
     async def on_msg(self, msg: Message):
-        self.log.info("on_msg: %s", msg)
+        self.log.debug("on_msg: %s", msg)
         await self.ws.send_json(msg)
 
     async def on_shutdown(self):
         if self.is_connected:
-            self.ws.close()
+            await self.ws.close()
 
     async def recv_commands(self):
         self.log.info('recv_commands: ...')
         try:
             while self.is_connected:
                 cmd = await self.ws.receive_json()
-                self.log.info("cmd: %s", cmd)
+                self.log.debug("cmd: %s", cmd)
                 await self.send_cmd(cmd)
         except WebSocketDisconnect:
             self.log.info("disconnect")
         except Exception as ex:
             self.log.exception(" %s: %s", self.user_id, ex)
         finally:
-            if self.is_connected:
-                await self.ws.close()
             await super().on_shutdown()
