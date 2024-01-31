7e0f886201dafaf6735dea5125eb781a75ed985c
Bogdan Velikorodov <bogdan.velikorodov@ravvi.net>
2024-01-31 15:45:51 +0300

    Добавил логику для получения игроков в клубе на данный момет, расширил club_profile полем club_balance

--- ravvi_poker/db/deploy/002_club_profile_.sql
+++ ravvi_poker/db/deploy/002_club_profile_.sql
@@ -1,4 +1,4 @@
-ALTER TABLE ONLY public.club_profile ADD COLUMN tables_сount INT DEFAULT 0, ADD COLUMN players_online INT DEFAULT 0;
+ALTER TABLE ONLY public.club_profile ADD COLUMN tables_сount INT DEFAULT 0, ADD COLUMN players_online INT DEFAULT 0, ADD COLUMN club_balance numeric(20,4) DEFAULT 0;
 
 CREATE FUNCTION public.table_close_trg_proc() RETURNS trigger
     AS $$
@@ -20,7 +20,27 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
+CREATE FUNCTION public.players_online_trg_proc() RETURNS trigger
+    AS $$
+DECLARE
+    club_id_var bigint;
+BEGIN
+    -- Получаем club_id из table_profile по id
+    SELECT club_id INTO club_id_var FROM public.table_profile WHERE id = NEW.table_id;
+
+    IF NEW.created_ts IS NOT NULL AND NEW.closed_ts IS NULL THEN
+        UPDATE public.club_profile SET players_online = players_online + 1 WHERE id = club_id_var;
+    ELSIF NEW.created_ts IS NOT NULL AND NEW.closed_ts IS NOT NULL THEN
+        UPDATE public.club_profile SET players_online = players_online - 1 WHERE id = club_id_var;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER players_online_trg AFTER INSERT OR UPDATE OF created_ts, closed_ts ON public.table_session FOR EACH ROW EXECUTE FUNCTION public.players_online_trg_proc();
 CREATE TRIGGER table_open_trg AFTER INSERT OR UPDATE OF created_ts ON public.table_profile FOR EACH ROW EXECUTE FUNCTION public.table_open_trg_proc();
 CREATE TRIGGER table_closed_trg AFTER UPDATE OF closed_ts ON public.table_profile FOR EACH ROW EXECUTE FUNCTION public.table_close_trg_proc();
-
 
