33abbd268b4009258671d795209977cc3f61a201
Bogdan Velikorodov <bogdan.velikorodov@ravvi.net>
2024-02-09 10:22:09 +0300

    Обработка timezone

--- ravvi_poker/api/clubs.py
+++ ravvi_poker/api/clubs.py
@@ -581,7 +581,7 @@ async def v1_user_account(club_id: int, session_uuid: SessionUUID):
                 game_types.append(game.game_type)
                 game_subtype.append(game.game_subtype)
 
-        winning_row = await db.get_statistics_about_winning(1002, date_now) #account.id Todo замени статичный id на account.id
+        winning_row = await db.get_statistics_about_winning(account.id, date_now) #account.id Todo замени статичный id на account.id
         sum_all_buyin = sum([float(value) for value in [row.txn_value for row in winning_row if row.txn_type == 'BUYIN']])
         sum_all_cashout = sum([float(value) for value in [row.txn_value for row in winning_row if row.txn_type == 'CASHOUT']])
         winning = sum_all_cashout - abs(sum_all_buyin)
