46a6014bb971e18298cefa6b598dc656a3f6ed88
Bogdan Velikorodov <bogdan.velikorodov@ravvi.net>
2024-02-20 10:33:17 +0300

    Поправил ошибку с sender_id в истории клуба, добавил запись в бд id стола на котором произошла транзакция

--- ravvi_poker/api/info.py
+++ ravvi_poker/api/info.py
@@ -107,11 +107,12 @@ async def v1_get_history_trx(club_id: int, session_uuid: SessionUUID):
         txn_list = []
         for txn in txn_history:
             txn = txn._asdict()
+            sender_account = await db.get_club_member(member_id=txn['sender_id'])
             if txn['txn_type'] not in ["BUYIN", "CASHOUT"]:
                 try:
-                    username = (await db.get_user(txn['sender_id'])).name
-                    role = (await db.find_account(user_id=txn['sender_id'], club_id=club_id)).user_role
-                    image_id = (await db.get_user(txn['sender_id'])).image_id
+                    username = (await db.get_user(sender_account.user_id)).name
+                    role = (await db.find_account(user_id=sender_account.user_id, club_id=club_id)).user_role
+                    image_id = (await db.get_user(sender_account.user_id)).image_id
                 except (ValueError, AttributeError):
                     username = "Undefined username"
                     role = "Undefined role"
@@ -129,7 +130,7 @@ async def v1_get_history_trx(club_id: int, session_uuid: SessionUUID):
                 )
                 txn_list.append(txn_manual)
             else:
-                table_info = await db.get_table(txn['sender_id'])
+                table_info = await db.get_table(txn['props'].get('table_id'))
                 txn_table = TxnHistoryOnTable(
                     table_name=table_info.table_name,
                     table_id=table_info.id,
