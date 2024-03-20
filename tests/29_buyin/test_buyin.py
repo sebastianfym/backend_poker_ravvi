94f6fa728a05650527662c9d055c8fd7aa8dcba1
Nikita Khasanov <nikita.hasanov@ravvi.net>
2024-03-20 11:31:17 +0300

    начало теста

--- tests/29_buyin/test_buyin.py
+++ tests/29_buyin/test_buyin.py
@@ -1,2 +1,15 @@
+import pytest
+
+from ravvi_poker.client.client import PokerClient
+
+
 class TestBuyin:
-    pass
+    @pytest.mark.asyncio
+    async def test_base(self):
+        client = PokerClient()
+
+        async with client:
+            await client.auth_register()
+            new_club = await client.create_club()
+            new_table = await client.create_table(club_id=new_club.id, table_type="RG", table_seats=2, game_type="NLH",
+                                                  game_subtype="REGULAR")
