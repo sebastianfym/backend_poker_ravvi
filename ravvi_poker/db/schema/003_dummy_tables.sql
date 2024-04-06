f4cca1cf691e8c98f8ba85445f9b3317562dec6c
Alexander Keda <alexander.keda@ravvi.net>
2024-04-06 20:39:43 +0300

    lobby tables params fix

--- ravvi_poker/db/schema/003_dummy_tables.sql
+++ ravvi_poker/db/schema/003_dummy_tables.sql
@@ -1,10 +1,10 @@
 insert into table_profile 
 (id, club_id, table_type, table_seats, game_type, game_subtype, props, n_bots) 
 VALUES
-(11, 0, 'RG', 9, 'NLH', 'REGULAR', '{}', 3),
-(12, 0, 'RG', 9, 'NLH', 'AOF', '{}', 3),
-(13, 0, 'RG', 9, 'NLH', '3-1', '{"drop_card_round": "PREFLOP"}', 3),
-(14, 0, 'RG', 9, 'NLH', '6+', '{}', 3),
-(21, 0, 'RG', 8, 'PLO', 'PLO4', '{}', 3),
-(22, 0, 'RG', 7, 'PLO', 'PLO5', '{}', 3),
-(23, 0, 'RG', 6, 'PLO', 'PLO6', '{}', 3);
+(11, 0, 'RG', 9, 'NLH', 'REGULAR', '{"buyin_min": 10}', 3),
+(12, 0, 'RG', 9, 'NLH', 'AOF', '{"buyin_min": 10}', 3),
+(13, 0, 'RG', 9, 'NLH', '3-1', '{"buyin_min": 10, "drop_card_round": "PREFLOP"}', 3),
+(14, 0, 'RG', 9, 'NLH', '6+', '{"buyin_min": 10}', 3),
+(21, 0, 'RG', 8, 'PLO', 'PLO4', '{"buyin_min": 10}', 3),
+(22, 0, 'RG', 7, 'PLO', 'PLO5', '{"buyin_min": 10}', 3),
+(23, 0, 'RG', 6, 'PLO', 'PLO6', '{"buyin_min": 10}', 3);
